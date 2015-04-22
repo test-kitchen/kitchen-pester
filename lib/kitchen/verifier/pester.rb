a3da411fa5e20214ca04189ee0a6b9cf06332204
Steven Murawski <steven.murawski@gmail.com>
2015-04-22 08:20:01 -0500

    reverting that change

--- lib/kitchen/verifier/pester.rb
+++ lib/kitchen/verifier/pester.rb
@@ -107,10 +107,8 @@ module Kitchen
         return if local_suite_files.empty?
 
         wrap_shell_code(Util.outdent!(<<-CMD))
-          if (-not (test-path c:/tests)) {mkdir c:/tests}
-          mv "#{File.join(config[:root_path],'suites/pester/' )}" -Dest c:/tests/ -force
-          cd c:/tests
-          invoke-pester -EnableExit
+          cd "#{File.join(config[:root_path],'suites/pester/' )}"
+          invoke-pester
         CMD
       end
 
