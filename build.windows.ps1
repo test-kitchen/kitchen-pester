dd34f37dbc613ed6a3e3bc4e3917272082551c95
gaelcolas <gaelcolas@synedgy.com>
2021-06-15 18:25:16 +0200

    updating the build windows to do without chef workstation (much quicker)
    Signed-off-by: gaelcolas <gaelcolas@synedgy.com>

--- build.windows.ps1
+++ build.windows.ps1
@@ -1,10 +1,14 @@
 #! 
 # https://www.sitepoint.com/creating-your-first-gem/
-chef gem build $PSScriptRoot/kitchen-pester.gemspec
+gem build $PSScriptRoot/kitchen-pester.gemspec
 
-rm -Force ~/appdata/local/chefdk/gem/ruby/*/cache/kitchen-pester-*.gem
-rm -Force ~/appdata/local/chefdk/gem/ruby/*/specifications/kitchen-dsc-*.gemspec
-rm -Force ~/appdata/local/chefdk/gem/ruby/*/gems/kitchen-pester-* -Recurse
+rm -EA SilentlyContinue -Force ~/appdata/local/chefdk/gem/ruby/*/cache/kitchen-pester-*.gem
+rm -EA SilentlyContinue -Force ~/appdata/local/chefdk/gem/ruby/*/specifications/kitchen-dsc-*.gemspec
+rm -EA SilentlyContinue -Force ~/appdata/local/chefdk/gem/ruby/*/gems/kitchen-pester-* -Recurse
 
-chef gem install (gi $PSScriptRoot/kitchen-pester-*.gem).Name
+rm -EA SilentlyContinue -Force C:\tools\ruby30\lib\ruby\gems\*\cache\kitchen-pester-*.gem
+rm -EA SilentlyContinue -Force C:\tools\ruby30\lib\ruby\gems\*\gems\kitchen-pester- -Recurse
+rm -EA SilentlyContinue -Force C:\tools\ruby30\lib\ruby\gems\*\specifications\kitchen-pester-*.gemspec
+
 Get-Date
+gem install (gi $PSScriptRoot/kitchen-pester-*.gem).Name
