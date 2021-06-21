3abdef8f84b222de70a703538e76233dfdd28c86
gaelcolas <gaelcolas@synedgy.com>
2021-06-21 12:23:50 +0200

    fixed comments
    Signed-off-by: gaelcolas <gaelcolas@synedgy.com>

--- build.windows.ps1
+++ build.windows.ps1
@@ -1,14 +1,23 @@
+param (
+    [Parameter()]
+    [String[]]
+    $FilesAndFoldersToClean = @(
+        '~/appdata/local/chefdk/gem/ruby/*/cache/kitchen-pester-*.gem'
+        '~/appdata/local/chefdk/gem/ruby/*/specifications/kitchen-dsc-*.gemspec'
+        '~/appdata/local/chefdk/gem/ruby/*/gems/kitchen-pester-*'
+        'C:\tools\ruby30\lib\ruby\gems\*\cache\kitchen-pester-*.gem'
+        'C:\tools\ruby30\lib\ruby\gems\*\gems\kitchen-pester-*'
+        'C:\tools\ruby30\lib\ruby\gems\*\specifications\kitchen-pester-*.gemspec'
+    )
+)
 #! 
 # https://www.sitepoint.com/creating-your-first-gem/
 gem build $PSScriptRoot/kitchen-pester.gemspec
 
-rm -EA SilentlyContinue -Force ~/appdata/local/chefdk/gem/ruby/*/cache/kitchen-pester-*.gem
-rm -EA SilentlyContinue -Force ~/appdata/local/chefdk/gem/ruby/*/specifications/kitchen-dsc-*.gemspec
-rm -EA SilentlyContinue -Force ~/appdata/local/chefdk/gem/ruby/*/gems/kitchen-pester-* -Recurse
+$FilesAndFoldersToClean | ForEach-Object -Process {
+    Remove-Item -ErrorAction SilentlyContinue -Force -Recurse -Path $_
+}
 
-rm -EA SilentlyContinue -Force C:\tools\ruby30\lib\ruby\gems\*\cache\kitchen-pester-*.gem
-rm -EA SilentlyContinue -Force C:\tools\ruby30\lib\ruby\gems\*\gems\kitchen-pester- -Recurse
-rm -EA SilentlyContinue -Force C:\tools\ruby30\lib\ruby\gems\*\specifications\kitchen-pester-*.gemspec
 
 Get-Date
-gem install (gi $PSScriptRoot/kitchen-pester-*.gem).Name
+gem install (Get-Item -Path $PSScriptRoot/kitchen-pester-*.gem).Name
